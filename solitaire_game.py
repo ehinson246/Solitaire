34ef1c0c4cb9e2a56a294e6dbd280c4b895a331b
ethan hinson <ethan.hinson@gmail.com>
2024-03-10 17:39:47 -0400

    v0.3.1: Added functions and fixed copied lists Added functions that create the piles on the field as well as the face up card that goes on top. Also chaged lists that used the .copy() method to instead be deep copies so that they would be unique individual lists that could be edited without altering the other list they were copied from.

--- solitaire_game.py
+++ solitaire_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import random
+import copy
 
 FULL_DECK_AMOUNT = 52
 FIELD_CARD_AMOUNT = 28
@@ -23,18 +24,18 @@ def generate_shuffled_deck():
     return shuffled_deck
 
 def get_starting_hand(deck):
-    starting_hand = deck.copy()
+    starting_hand = copy.deepcopy(deck)
     while len(starting_hand) > STARTING_HAND_AMOUNT:
         starting_hand.pop(0)
     return starting_hand
 
 def get_field_cards(deck):
-    field_cards = deck.copy()
+    field_cards = copy.deepcopy(deck)
     while len(field_cards) > FIELD_CARD_AMOUNT:
         field_cards.pop(-1)
     return field_cards
 
-def create_field_piles(field_cards):
+def create_initial_field_piles(field_cards):
     pile_1 = []
     pile_2 = []
     pile_3 = []
@@ -42,34 +43,63 @@ def create_field_piles(field_cards):
     pile_5 = []
     pile_6 = []
     pile_7 = []
-    field_piles = [pile_1, pile_2, pile_3, pile_4, pile_5, pile_6, pile_7]
+    initial_field_piles = [pile_1, pile_2, pile_3, pile_4, pile_5, pile_6, pile_7]
     pile_index = 0
     field_card_index = 0
     while pile_index < NUMBER_OF_FIELD_PILES:
-        current_pile = field_piles[pile_index]
+        current_pile = initial_field_piles[pile_index]
         pile_amount = pile_index + 1
         while len(current_pile) < pile_amount:
             next_card = field_cards[field_card_index]
-            field_card_index += 1
             current_pile.append(next_card)
+            field_card_index += 1
         pile_index += 1
-    return field_piles
+    return initial_field_piles
 
-def deal_starting_position():
+def create_field_stacks(initial_field_piles):
+    stack_1 = []
+    stack_2 = []
+    stack_3 = []
+    stack_4 = []
+    stack_5 = []
+    stack_6 = []
+    stack_7 = []
+    field_stacks = [stack_1, stack_2, stack_3, stack_4, stack_5, stack_6, stack_7]
+    index = 0
+    for stack in field_stacks:
+        pile_index = index
+        top_card = initial_field_piles[pile_index][0]
+        stack.append(top_card)
+        index += 1
+    return field_stacks
+
+def finalize_field_piles(initial_field_piles):
+    finalized_field_piles = copy.deepcopy(initial_field_piles)
+    for pile in finalized_field_piles:
+        pile.pop(0)
+    return finalized_field_piles
+
+def create_starting_position_info():
     deck = generate_shuffled_deck()
     starting_hand = get_starting_hand(deck)
     field_cards = get_field_cards(deck)
-    field_piles = create_field_piles(field_cards)
-    return (deck, starting_hand, field_cards, field_piles)
+    initial_field_piles = create_initial_field_piles(field_cards)
+    finalized_field_piles = finalize_field_piles(initial_field_piles)
+    field_stacks = create_field_stacks(initial_field_piles)
+    return (deck, starting_hand, field_cards, initial_field_piles, finalized_field_piles, field_stacks)
 
-position = deal_starting_position()
+position = create_starting_position_info
 
 deck = position[0]
 starting_hand = position[1]
 field_cards = position[2]
-field_piles = position[3]
+initial_field_piles = position[3]
+finalized_field_piles = position[4]
+field_stacks = position[5]
 
 print(deck)
 print(field_cards)
 print(starting_hand)
-print(field_piles)
+print(initial_field_piles)
+print(field_stacks)
+print(finalized_field_piles)
